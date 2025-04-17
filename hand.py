1619678ab1307e074b39ae282a5299b40fd6b0f2
w-ibl <jeffrey.chong@live.com>
2025-04-17 11:07:08 +0100

    Update hand.py

--- hand.py
+++ hand.py
@@ -1,41 +1,24 @@
 import cv2
-import cvzone
-from cvzone.HandTrackingModule import HandDetector
-import time
-
-import cv2
-from cvzone.ObjectDetectionModule import Detector
-
-# Initialize the detector (using YOLO model)
-detector = Detector()
-
-# Open the webcam
-cap = cv2.VideoCapture(0)
-
-while True:
-    # Read frame from the webcam
-    ret, frame = cap.read()
-    
-    if not ret:
-        break
-
-    # Detect persons and other objects in the frame
-    img, objects = detector.update(frame)
-    
-    # Loop through the detected objects and draw bounding boxes around persons
-    for obj in objects:
-        if obj["name"] == "person":  # You can also check for other objects here
-            cv2.rectangle(img, obj["top_left"], obj["bottom_right"], (255, 0, 0), 2)
-            cv2.putText(img, obj["name"], obj["top_left"], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    
-    # Display the frame with detected persons
-    cv2.imshow("Person Detection", img)
-    
-    # Break if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the webcam and close the windows
-cap.release()
-cv2.destroyAllWindows()
-
+import face_recognition
+ 
+imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
+imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file('ImagesBasic/Bill gates.jpg')
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+ 
+faceLoc = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+ 
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+ 
+results = face_recognition.compare_faces([encodeElon],encodeTest)
+faceDis = face_recognition.face_distance([encodeElon],encodeTest)
+print(results,faceDis)
+cv2.putText(imgTest,f'{results} {round(faceDis[0],2)}',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+ 
+cv2.imshow('Elon Musk',imgElon)
+cv2.imshow('Elon Test',imgTest)
+cv2.waitKey(0)
